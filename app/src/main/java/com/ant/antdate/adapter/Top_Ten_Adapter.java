db9512f2e26365a633156439b0741a4c3b6e9678
whateverz <13581714368@163.com>
2020-06-02 15:30:27 +0800

    广场

--- app/src/main/java/com/ant/antdate/adapter/Top_Ten_Adapter.java
+++ app/src/main/java/com/ant/antdate/adapter/Top_Ten_Adapter.java
@@ -5,51 +5,38 @@ import android.database.DataSetObserver;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import com.ant.antdate.fragment.SquareFragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Top_Ten_Adapter extends FragmentStatePagerAdapter {
-    private ArrayList<String> mList;
-    private ArrayList<Fragment> fragmentsList;
-
-    protected Context mContext;
-
-    public Top_Ten_Adapter(Context context, FragmentManager fm, ArrayList<String> arrayList) {
+public class Top_Ten_Adapter extends FragmentPagerAdapter {
+    private List<Fragment> fragmentList = new ArrayList<Fragment>();
+    private String[] strings = new String[]{"top10","B","C","D"};
+    public Top_Ten_Adapter(@NonNull FragmentManager fm,List<Fragment> fragmentList) {
         super(fm);
-        this.mContext = context;
-        this.mList = arrayList;
     }
-    @Override
-    public int getCount() {
-        // TODO Auto-generated method stub
-        return mList.size();
-    }
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return mList.get(position);
-    }
+
     @NonNull
     @Override
     public Fragment getItem(int position) {
-
-        return null;
+        return fragmentList.get(position);
     }
 
     @Override
-    public boolean isViewFromObject(View arg0, Object arg1) {
-        // TODO Auto-generated method stub
-        return false;
+    public int getCount() {
+        return 2;
     }
 
+    @Nullable
     @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {
-        if (observer != null) {
-            super.unregisterDataSetObserver(observer);
-        }
+    public CharSequence getPageTitle(int position) {
+        return strings[position];
     }
 }
--- app/src/main/java/com/ant/antdate/fragment/SquareFragment.java
+++ app/src/main/java/com/ant/antdate/fragment/SquareFragment.java
@@ -1,48 +1,114 @@
 package com.ant.antdate.fragment;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.ant.antdate.R;
+import com.ant.antdate.adapter.ExamplePagerAdapter;
 import com.ant.antdate.adapter.Top_Ten_Adapter;
 import com.ant.antdate.base.BaseFragment;
 import com.google.android.material.tabs.TabLayout;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.badge.BadgePagerTitleView;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import lib.frame.module.ui.BindView;
 
 public class SquareFragment extends BaseFragment {
 
-    private TabLayout tablayout;
+    MagicIndicator magicIndicator;
+
     private  ViewPager viewpager;
+    static final int NUM_ITEMS = 4;
+    private List<Fragment> fragmentList = new ArrayList<Fragment>();
     private ArrayList<String> mTabs = new ArrayList<>();
     private Top_Ten_Adapter top_ten_adapter;
+    private static final String[] CHANNELS = new String[]{"TOP.10", "蚂蚁热区"};
+    private List<String> mDataList = Arrays.asList(CHANNELS);
+    private ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
+
     private View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_squre,container,false);
         viewpager = view.findViewById(R.id.viewpager);
-        tablayout = view.findViewById(R.id.tablayout);
-      //  initTabView();
+        magicIndicator = view. findViewById(R.id.magic_indicator);
+        viewpager.setAdapter(mExamplePagerAdapter);
+        initMagicIndicator4();
+
         return view;
     }
-    private void initTabView() {
-        mTabs.add("top10");
-        mTabs.add("蚂蚁热区");
-        top_ten_adapter = new Top_Ten_Adapter(getContext(), getChildFragmentManager(), mTabs);
 
-        viewpager.setAdapter(top_ten_adapter);
+    private void initMagicIndicator4() {
+        CommonNavigator commonNavigator = new CommonNavigator(getContext());
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
 
-        viewpager.setOffscreenPageLimit(0);
+            @Override
+            public int getCount() {
+                return mDataList.size();
+            }
 
-        tablayout.setupWithViewPager(viewpager);
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                BadgePagerTitleView badgePagerTitleView = new BadgePagerTitleView(context);
+
+                SimplePagerTitleView simplePagerTitleView = new ColorTransitionPagerTitleView(context);
+                simplePagerTitleView.setNormalColor(Color.GRAY);
+                simplePagerTitleView.setSelectedColor(getResources().getColor(R.color.theme_color_green));
+                simplePagerTitleView.setText(mDataList.get(index));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewpager.setCurrentItem(index);
+                    }
+                });
+                badgePagerTitleView.setInnerPagerTitleView(simplePagerTitleView);
+
+                return badgePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator linePagerIndicator = new LinePagerIndicator(context);
+                linePagerIndicator.setColors(getResources().getColor(R.color.theme_color_green));
+                linePagerIndicator.setLineWidth(90);
+                return linePagerIndicator;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        LinearLayout titleContainer = commonNavigator.getTitleContainer(); // must after setNavigator
+        titleContainer.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        titleContainer.setDividerDrawable(new ColorDrawable() {
+            @Override
+            public int getIntrinsicWidth() {
+                return UIUtil.dip2px(getContext(), 5);
+            }
+        });
+        ViewPagerHelper.bind(magicIndicator, viewpager);
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
